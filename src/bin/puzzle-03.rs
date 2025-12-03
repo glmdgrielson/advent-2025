48ea5fc2dcf94774c0b5caaea046bcf0948b12a8
glmdgrielson <zapperchamp1@gmail.com>
2025-12-03 07:47:46 -0500

    Run rustfmt on skeleton of Puzzle 03

--- src/bin/puzzle-03.rs
+++ src/bin/puzzle-03.rs
@@ -3,7 +3,7 @@
 //!
 //! Alright let's get our power back online!
 
-use advent_2025::{Puzzle, AdventError, read_file};
+use advent_2025::{read_file, AdventError, Puzzle};
 
 #[derive(Clone, Debug)]
 // This is never going to store more than a single digit in each cell,
@@ -38,14 +38,13 @@ mod test {
 
     use std::sync::LazyLock;
 
-    static TEST_INPUT: LazyLock<String> = LazyLock::new(|| {
-        read_file("input/puzzle03-test.txt").expect("Could not read input")
-    });
+    static TEST_INPUT: LazyLock<String> =
+        LazyLock::new(|| read_file("input/puzzle03-test.txt").expect("Could not read input"));
 
     #[test]
     fn parse_input() {
         let data = Banks::parse_input(&TEST_INPUT).expect("could not parse input");
 
-        assert_eq!(data.0[0][0..9], vec![9,8,7,6,5,4,3,2,1]);
+        assert_eq!(data.0[0][0..9], vec![9, 8, 7, 6, 5, 4, 3, 2, 1]);
     }
 }
