f555f534876fcb6f7f777c7fc733ab94dfa87c80
glmdgrielson <zapperchamp1@gmail.com>
2025-12-06 14:24:52 -0500

    Solve part 2 of Puzzle 06
    BUS THAT STUPID PUZZLE SO HARD. This wasn't really _hard_ per se, as so
    much as the twist added in part two just being really ANNOYING to deal
    with.

--- src/bin/puzzle-06.rs
+++ src/bin/puzzle-06.rs
@@ -48,8 +48,25 @@ impl Puzzle for Worksheet {
             })
             .collect::<Result<Vec<_>, AdventError>>()?;
 
+        // Before we assemble the grid, we need to make sure every line
+        // has the same length, or [Grid::push_row] will panic.
+        // So we find the longest line...
+        let Some(line_len) = sheet.into_iter().max_by_key(|line| line.len()) else {
+            return Err(AdventError::Data("file should have operands".to_string()));
+        };
+        // And get its length.
+        let max_len = line_len.len();
         let sheet = sheet
             .iter()
+            // Make sure all of the lines are the same length.
+            .map(|line| {
+                let mut new_line = line.to_string();
+                for _ in 0..(max_len - line.len()) {
+                    new_line.push(' ');
+                }
+                new_line
+            })
+            // Because `Grid` nees them all the same length.
             .fold(Grid::new(0, 0, vec![]), |mut grid, line| {
                 grid.push_row(line.chars().collect());
                 grid
@@ -62,16 +79,21 @@ impl Puzzle for Worksheet {
 
     /// Find the sum of all of the correct answers.
     fn part_one(&self) -> Result<String, AdventError> {
-        todo!()
-        // let sum = self.0.iter().map(|equation| match equation.operation {
-        //     Operation::Add => equation.operands.iter().sum::<u64>(),
-        //     Operation::Mul => equation.operands.iter().product::<u64>(),
-        // }).sum::<u64>();
-        // Ok(sum.to_string())
+        let equations = self.parse_one()?;
+        let sum = equations.iter().map(|equation| match equation.operation {
+            Operation::Add => equation.operands.iter().sum::<u64>(),
+            Operation::Mul => equation.operands.iter().product::<u64>(),
+        }).sum::<u64>();
+        Ok(sum.to_string())
     }
 
     fn part_two(&self) -> Result<String, AdventError> {
-        todo!()
+        let equations = self.parse_two()?;
+        let sum = equations.iter().map(|equation| match equation.operation {
+            Operation::Add => equation.operands.iter().sum::<u64>(),
+            Operation::Mul => equation.operands.iter().product::<u64>(),
+        }).sum::<u64>();
+        Ok(sum.to_string())
     }
 }
 
@@ -89,7 +111,10 @@ impl Worksheet {
                 grid.push_row(line);
                 Ok(grid)
             })?;
-        assert_eq!(grid.width(), self.operations.len());
+        //assert_eq!(grid.width(), self.operations.len());
+        if grid.width() != self.operations.len() {
+            return Err(AdventError::Parse("improperly formed worksheet".to_string()));
+        }
 
         let sheet = grid
             .columns()
@@ -109,60 +134,38 @@ impl Worksheet {
             }).collect::<Result<Vec<_>, AdventError>>()?;
         Ok(sheet)
     }
-}
 
-fn parse_input_one(file: &str) -> Result<Vec<Equation>, AdventError> {
-    // Assemble the worksheet. `Grid` is a useful type to handle
-    // the two dimensional nature of the sheet, and it was already
-    // a dependency anyway because of earlier puzzles.
-    let grid: Grid<&str> = file
-        .lines()
-        .fold(Grid::new(0, 0, vec![]), |mut grid, line| {
-            let line = line.split_ascii_whitespace().collect();
-            grid.push_row(line);
-            grid
-        });
+    fn parse_two(&self) -> Result<Vec<Equation>, AdventError> {
+        let columns = self.sheet.columns().map(|col| {
+            self.sheet.column_iter(col).collect::<String>()
+        }).map(|num| num.trim_ascii().to_owned()).collect::<Vec<_>>();
 
-    // Convert each column into a proper equation.
-    let sheet = grid
-        .columns()
-        .map(|idx| {
-            // `Grid` only indirectly lets me access a column,
-            // so I do this to actually get at the data I want.
-            // `.cloned()` lets me get rid of an indirection.
-            let equation = grid.column_iter(idx).cloned().collect::<Vec<_>>();
-            // The last element of each equation is special, so
-            // make sure that it's separate from all of the operands.
-            let Some((operation, operands)) = equation.split_last() else {
-                return Err(AdventError::Parse("empty column found".to_string()));
-            };
-
-            // The operation should realistically be only one of two
-            // things, so we check for that here and error out if we
-            // get something weird.
-            let operation = match *operation {
-                "+" => Operation::Add,
-                "*" => Operation::Mul,
-                op => return Err(AdventError::Parse(format!("invalid operation {0}", op))),
-            };
-            // Everything else in the equation should be a number
-            // so we check for that and perform the conversion.
-            let operands = operands
-                .iter()
-                .map(|num| {
-                    num.parse::<u64>()
-                        .map_err(|_| AdventError::Parse(format!("invalid operand {0}", num)))
+        let operands = columns.split(|col| col.is_empty()).collect::<Vec<_>>();
+        // We need to make sure we have one set of operands
+        // for every operation we already had.
+        if operands.len() != self.operations.len() {
+            return Err(AdventError::Data("improperly formed worksheet".to_string()))
+        };
+        let sheet = operands
+            .iter()
+            .enumerate()
+            .map(|(idx, nums)| {
+                let nums = nums
+                    .iter()
+                    .map(|num| {
+                        num.parse::<u64>()
+                            .map_err(|_| AdventError::Data(format!("invalid operand {0}", num)))
+                    })
+                    .collect::<Result<Vec<_>, AdventError>>()?;
+                let operation = self.operations[idx];
+                Ok(Equation {
+                    operands: nums,
+                    operation,
                 })
-                .collect::<Result<Vec<_>, AdventError>>()?;
-
-            // Finally return the equation to go into the final result.
-            Ok(Equation {
-                operands,
-                operation,
             })
-        })
-        .collect::<Result<Vec<_>, AdventError>>()?;
-    Ok(sheet)
+            .collect::<Result<Vec<_>, AdventError>>()?;
+        Ok(sheet)
+    }
 }
 
 fn main() -> Result<(), AdventError> {
@@ -171,6 +174,7 @@ fn main() -> Result<(), AdventError> {
     let data = Worksheet::parse_input(&file)?;
 
     println!("The grand total solution is {0}", data.part_one()?);
+    println!("The correctly read solution is {0}", data.part_two()?);
     Ok(())
 }
 
@@ -204,5 +208,30 @@ mod test {
 
         let answer = data.part_one().unwrap();
         assert_eq!(answer, "4277556");
+    }
+
+    #[test]
+    fn parse_two() {
+        let data = Worksheet::parse_input(&TEST_INPUT).expect("could not parse input file");
+        let sheet = data.parse_two().unwrap();
+
+        assert_eq!(
+            sheet[0],
+            // This is technically the _last_ of the operations,
+            // since we're reading right to left, but at this
+            // point, that's a technicality I can't be bothered with.
+            Equation {
+                operands: vec![1, 24, 356],
+                operation: Operation::Mul,
+            }
+        );
+    }
+
+    #[test]
+    fn part_two() {
+        let data = Worksheet::parse_input(&TEST_INPUT).expect("could not parse input file");
+
+        let answer = data.part_two().unwrap();
+        assert_eq!(answer, "3263827");
     }
 }
