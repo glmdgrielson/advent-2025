bc598346d7087ce9f6b55cd2a8da2050161c95d8
glmdgrielson <zapperchamp1@gmail.com>
2025-12-07 14:07:26 -0500

    Run rustfmt on Puzzle 07

--- src/bin/puzzle-07.rs
+++ src/bin/puzzle-07.rs
@@ -25,9 +25,9 @@ impl Puzzle for Manifold {
     /// representing empty space, or a caret (`|`) representing
     /// a beam splitter.
     fn parse_input(file: &str) -> Result<Self, AdventError> {
-        let grid = file
-            .lines()
-            .try_fold(Grid::new(0, 0, Vec::new()), |mut grid, line| -> Result<_, AdventError> {
+        let grid = file.lines().try_fold(
+            Grid::new(0, 0, Vec::new()),
+            |mut grid, line| -> Result<_, AdventError> {
                 let line = line
                     .chars()
                     .map(|ch| match ch {
@@ -39,7 +39,8 @@ impl Puzzle for Manifold {
                     .collect::<Result<Vec<Cell>, AdventError>>()?;
                 grid.push_row(line);
                 Ok(grid)
-            })?;
+            },
+        )?;
         Ok(Manifold(grid))
     }
 
@@ -50,8 +51,16 @@ impl Puzzle for Manifold {
     /// Horne moment here.
     fn part_one(&self) -> Result<String, AdventError> {
         // Sanity check here.
-        if self.0.row_iter(0).filter(|&cell| *cell == Cell::Start).count() != 1 {
-            return Err(AdventError::Data("first row does not have exactly one start".to_string()));
+        if self
+            .0
+            .row_iter(0)
+            .filter(|&cell| *cell == Cell::Start)
+            .count()
+            != 1
+        {
+            return Err(AdventError::Data(
+                "first row does not have exactly one start".to_string(),
+            ));
         }
         let Some(start) = self.0.row_iter(0).position(|&cell| cell == Cell::Start) else {
             unreachable!("should have just checked for start's existence");
@@ -63,7 +72,9 @@ impl Puzzle for Manifold {
         let mut beams = HashSet::new();
         // Add the column of our initial starting position.
         beams.insert(start);
-        let beams: Result<_, AdventError> = self.0.rows()
+        let beams: Result<_, AdventError> = self
+            .0
+            .rows()
             .skip(1) // ignore the first row
             .try_fold((0, beams), |(splits, beams), row| {
                 let mut new_beams = beams.clone();
@@ -88,9 +99,11 @@ impl Puzzle for Manifold {
                             if col != self.0.width() - 1 {
                                 new_beams.insert(col + 1);
                             }
-                        },
-                        Some(Cell::Start) => return Err(AdventError::Data("multiple starts found".to_string())),
-                        None => unreachable!("escaped grid somehow {0}:{1}", row, col)
+                        }
+                        Some(Cell::Start) => {
+                            return Err(AdventError::Data("multiple starts found".to_string()))
+                        }
+                        None => unreachable!("escaped grid somehow {0}:{1}", row, col),
                     }
                 }
                 Ok((splits, new_beams))
@@ -102,42 +115,59 @@ impl Puzzle for Manifold {
 
     /// Find the total number of paths the particle can take.
     fn part_two(&self) -> Result<String, AdventError> {
-        if self.0.row_iter(0).filter(|&cell| *cell == Cell::Start).count() != 1 {
-            return Err(AdventError::Data("first row should contain exactly one start".to_string()));
+        if self
+            .0
+            .row_iter(0)
+            .filter(|&cell| *cell == Cell::Start)
+            .count()
+            != 1
+        {
+            return Err(AdventError::Data(
+                "first row should contain exactly one start".to_string(),
+            ));
         }
 
-        let init = self.0.row_iter(0).map(|cell| match cell {
-            Cell::Start => Ok(1),
-            Cell::Empty => Ok(0),
-            Cell::Split => Err(AdventError::Data("unreachable splitter".to_string())),
-        }).collect::<Result<Vec<usize>, AdventError>>()?;
+        let init = self
+            .0
+            .row_iter(0)
+            .map(|cell| match cell {
+                Cell::Start => Ok(1),
+                Cell::Empty => Ok(0),
+                Cell::Split => Err(AdventError::Data("unreachable splitter".to_string())),
+            })
+            .collect::<Result<Vec<usize>, AdventError>>()?;
 
-        let res: Result<_, AdventError> = self.0.rows()
+        let res: Result<_, AdventError> = self
+            .0
+            .rows()
             .skip(1) // ignore the first row
             .try_fold(init, |totals, row| {
                 let mut next = vec![0; totals.len()];
-                self.0.row_iter(row).enumerate().try_for_each(|(idx, &col)| {
-                    match col {
-                        Cell::Empty => {
-                            // Completely missed that I
-                            // zeroed out the initial row, oops.
-                            next[idx] += totals[idx]; 
-                        }
-                        Cell::Split => {
-                            next[idx] = 0;
-                            if idx != 0 {
-                                next[idx - 1] += totals[idx];
+                self.0
+                    .row_iter(row)
+                    .enumerate()
+                    .try_for_each(|(idx, &col)| {
+                        match col {
+                            Cell::Empty => {
+                                // Completely missed that I
+                                // zeroed out the initial row, oops.
+                                next[idx] += totals[idx];
                             }
-                            if idx != totals.len() - 1 {
-                                next[idx + 1] += totals[idx];
+                            Cell::Split => {
+                                next[idx] = 0;
+                                if idx != 0 {
+                                    next[idx - 1] += totals[idx];
+                                }
+                                if idx != totals.len() - 1 {
+                                    next[idx + 1] += totals[idx];
+                                }
+                            }
+                            Cell::Start => {
+                                return Err(AdventError::Data("multiple starts found".to_string()))
                             }
                         }
-                        Cell::Start => return Err(
-                            AdventError::Data("multiple starts found".to_string())
-                        ),
-                    }
-                    Ok(())
-                })?;
+                        Ok(())
+                    })?;
                 Ok(next)
             });
         let sum = res?.iter().sum::<usize>();
