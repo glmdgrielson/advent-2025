982a983a44a1b4e1bd67c55bdc9ae38175199353
glmdgrielson <zapperchamp1@gmail.com>
2025-12-09 17:00:51 -0500

    Solve part 2 of Puzzle 09

--- src/bin/puzzle-09.rs
+++ src/bin/puzzle-09.rs
@@ -8,8 +8,10 @@ use advent_2025::{read_file, Puzzle, AdventError};
 
 use itertools::Itertools;
 
+type Point = (u64, u64);
+
 #[derive(Clone, Debug)]
-struct Floor(Vec<(u64, u64)>);
+struct Floor(Vec<Point>);
 
 impl Puzzle for Floor {
     /// Input consists of a list of pairs of numbers,
@@ -43,8 +45,55 @@ impl Puzzle for Floor {
         }
     }
 
+    /// Find the largest rectangle bounded in the shape
+    /// implied by the connections between adjacent points.
+    ///
+    /// TODO: This takes a noticeable second, so I could
+    /// probably optimize this a bit, but that's for after
+    /// puzzles are done.
     fn part_two(&self) -> Result<String, AdventError> {
-        todo!()
+        let edges = self.0.iter()
+            .circular_tuple_windows()
+            .map(|(one, two)| Line {one: *one, two: *two})
+            .collect::<Vec<_>>();
+
+        let max = self.0.iter().tuple_combinations().filter_map(|(one, two)| {
+            if edges.iter().all(|line| line.out_of_bounds(*one, *two)) {
+                Some((one.0.abs_diff(two.0) + 1) * (one.1.abs_diff(two.1) + 1))
+            } else {
+                None
+            }
+        }).max();
+        if let Some(max) = max {
+            Ok(max.to_string())
+        } else {
+            Err(AdventError::Data("could not find bounded rectangle".to_string()))
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Line {
+    one: Point,
+    two: Point,
+}
+
+impl Line {
+    fn out_of_bounds(&self, p1: Point, p2: Point) -> bool {
+        let ax = p1.0.min(p2.0);
+        let bx = p1.0.max(p2.0);
+        let ay = p1.1.min(p2.1);
+        let by = p1.1.max(p2.1);
+
+        let x_min = self.one.0.min(self.two.0);
+        let x_max = self.one.0.max(self.two.0);
+        let y_min = self.one.1.min(self.two.1);
+        let y_max = self.one.1.max(self.two.1);
+
+        x_max <= ax
+        || x_min >= bx
+        || y_max <= ay
+        || y_min >= by
     }
 }
 
@@ -53,6 +102,7 @@ fn main() -> Result<(), AdventError> {
     let data = Floor::parse_input(&file)?;
 
     println!("The largest rectangle is {0}", data.part_one()?);
+    println!("The largest bounded rectangle is {0}", data.part_two()?);
     Ok(())
 }
 
@@ -78,5 +128,13 @@ mod test {
 
         let answer = data.part_one().unwrap();
         assert_eq!(answer, "50");
+    }
+
+    #[test]
+    fn part_two() {
+        let data = Floor::parse_input(&TEST_INPUT).expect("could not parse input file");
+
+        let answer = data.part_two().unwrap();
+        assert_eq!(answer, "24");
     }
 }
