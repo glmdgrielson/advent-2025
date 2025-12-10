0eff7353ef86069987c6fca3bf7583421c701059
glmdgrielson <zapperchamp1@gmail.com>
2025-12-10 16:15:47 -0500

    Format Puzzle 09

--- src/bin/puzzle-09.rs
+++ src/bin/puzzle-09.rs
@@ -4,7 +4,7 @@
 //! No, look down at the floor! It's better entertainment
 //! than the movie they're showing here, anyway!
 
-use advent_2025::{read_file, Puzzle, AdventError};
+use advent_2025::{read_file, AdventError, Puzzle};
 
 use itertools::Itertools;
 
@@ -17,24 +17,35 @@ impl Puzzle for Floor {
     /// Input consists of a list of pairs of numbers,
     /// representing coordinate points.
     fn parse_input(file: &str) -> Result<Self, AdventError> {
-        let pairs = file.lines().map(|line| {
-            let pair = line.split(',')
-                .map(|num| num.parse::<u64>()
-                    .map_err(|e| AdventError::Parse(format!("invalid coordinate {0}", e))))
-                .collect::<Result<Vec<_>, AdventError>>()?;
-            if pair.len() != 2 {
-                Err(AdventError::Parse(format!("invalid coordinate pair {0}", line)))
-            } else {
-                Ok((pair[0], pair[1]))
-            }
-        })
-        .collect::<Result<Vec<_>, AdventError>>()?;
+        let pairs = file
+            .lines()
+            .map(|line| {
+                let pair = line
+                    .split(',')
+                    .map(|num| {
+                        num.parse::<u64>()
+                            .map_err(|e| AdventError::Parse(format!("invalid coordinate {0}", e)))
+                    })
+                    .collect::<Result<Vec<_>, AdventError>>()?;
+                if pair.len() != 2 {
+                    Err(AdventError::Parse(format!(
+                        "invalid coordinate pair {0}",
+                        line
+                    )))
+                } else {
+                    Ok((pair[0], pair[1]))
+                }
+            })
+            .collect::<Result<Vec<_>, AdventError>>()?;
         Ok(Floor(pairs))
     }
 
     /// Find the biggest rectangle between two points on the floor.
     fn part_one(&self) -> Result<String, AdventError> {
-        let max = self.0.iter().tuple_combinations()
+        let max = self
+            .0
+            .iter()
+            .tuple_combinations()
             // Fix off by one errors.
             .map(|(one, two)| (one.0.abs_diff(two.0) + 1) * (one.1.abs_diff(two.1) + 1))
             .max();
@@ -52,22 +63,34 @@ impl Puzzle for Floor {
     /// probably optimize this a bit, but that's for after
     /// puzzles are done.
     fn part_two(&self) -> Result<String, AdventError> {
-        let edges = self.0.iter()
+        let edges = self
+            .0
+            .iter()
             .circular_tuple_windows()
-            .map(|(one, two)| Line {one: *one, two: *two})
+            .map(|(one, two)| Line {
+                one: *one,
+                two: *two,
+            })
             .collect::<Vec<_>>();
 
-        let max = self.0.iter().tuple_combinations().filter_map(|(one, two)| {
-            if edges.iter().all(|line| line.out_of_bounds(*one, *two)) {
-                Some((one.0.abs_diff(two.0) + 1) * (one.1.abs_diff(two.1) + 1))
-            } else {
-                None
-            }
-        }).max();
+        let max = self
+            .0
+            .iter()
+            .tuple_combinations()
+            .filter_map(|(one, two)| {
+                if edges.iter().all(|line| line.out_of_bounds(*one, *two)) {
+                    Some((one.0.abs_diff(two.0) + 1) * (one.1.abs_diff(two.1) + 1))
+                } else {
+                    None
+                }
+            })
+            .max();
         if let Some(max) = max {
             Ok(max.to_string())
         } else {
-            Err(AdventError::Data("could not find bounded rectangle".to_string()))
+            Err(AdventError::Data(
+                "could not find bounded rectangle".to_string(),
+            ))
         }
     }
 }
@@ -90,10 +113,7 @@ impl Line {
         let y_min = self.one.1.min(self.two.1);
         let y_max = self.one.1.max(self.two.1);
 
-        x_max <= ax
-        || x_min >= bx
-        || y_max <= ay
-        || y_min >= by
+        x_max <= ax || x_min >= bx || y_max <= ay || y_min >= by
     }
 }
 
@@ -112,8 +132,9 @@ mod test {
 
     use std::sync::LazyLock;
 
-    static TEST_INPUT: LazyLock<String> = LazyLock::new(
-        || read_file("src/input/puzzle09-test.txt").expect("could not read input file"));
+    static TEST_INPUT: LazyLock<String> = LazyLock::new(|| {
+        read_file("src/input/puzzle09-test.txt").expect("could not read input file")
+    });
 
     #[test]
     fn parse_input() {
