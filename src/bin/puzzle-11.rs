0be02c786fc1317a82280d2b2108d856da571c1b
glmdgrielson <zapperchamp1@gmail.com>
2025-12-11 12:15:12 -0500

    Solve part one 1 of Puzzle 11
    Oh Part 2 looks decently easy!

--- src/bin/puzzle-11.rs
+++ src/bin/puzzle-11.rs
@@ -5,6 +5,8 @@
 
 use advent_2025::{read_file, Puzzle, AdventError};
 
+use std::collections::hash_map::RandomState;
+
 use petgraph::prelude::DiGraph;
 use petgraph::algo::simple_paths::all_simple_paths;
 
@@ -32,6 +34,9 @@ impl Puzzle for Rack {
             let node = graph.add_node(node.to_string());
             node
         }).collect::<Vec<_>>();
+        // `out` doesn't link to any nodes, so add it to the graph manually.
+        graph.add_node("out".to_string());
+
         let neighbors = connections.iter().map(|(_, neighbors)| neighbors).collect::<Vec<_>>();
         for idx in 0..nodes.len() {
             let node = nodes[idx];
@@ -46,7 +51,15 @@ impl Puzzle for Rack {
     }
 
     fn part_one(&self) -> Result<String, AdventError> {
-        todo!()
+        let Some(you) = self.0.node_indices().find(|idx| self.0[*idx] == "you") else {
+            return Err(AdventError::Data(format!("root node not found: {0:?}", self.0)));
+        };
+        let Some(out) = self.0.node_indices().find(|idx| self.0[*idx] == "out") else {
+            return Err(AdventError::Data(format!("end node not found: {0:?}", self.0)));
+        };
+        
+        let paths = all_simple_paths::<Vec<_>, _, RandomState>(&self.0, you, out, 1, None).count();
+        Ok(paths.to_string())
     }
 
     fn part_two(&self) -> Result<String, AdventError> {
@@ -56,7 +69,9 @@ impl Puzzle for Rack {
 
 fn main() -> Result<(), AdventError> {
     let file = read_file("src/input/puzzle11.txt")?;
+    let data = Rack::parse_input(&file)?;
 
+    println!("The number of paths from 'you' to 'out' is {0}", data.part_one()?);
     Ok(())
 }
 
@@ -77,5 +92,13 @@ mod test {
         let you = data.0.node_indices().find(|node| data.0[*node] == "you")
             .expect("graph should have 'you' node");
         assert_eq!(data.0.neighbors(you).count(), 2);
+    }
+
+    #[test]
+    fn part_one() {
+        let data = Rack::parse_input(&TEST_INPUT).expect("could not parse input file");
+
+        let answer = data.part_one().unwrap();
+        assert_eq!(answer, "5");
     }
 }
